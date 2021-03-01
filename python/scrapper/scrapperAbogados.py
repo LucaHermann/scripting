c35f6bb9ac4852254cdbc8b0c2d5da45609d219a
Hermann Luca <lhermann@student.42.fr>
2021-03-01 22:51:08 +0100

    feat: scrapper 10lines

--- python/scrapper/scrapperAbogados.py
+++ python/scrapper/scrapperAbogados.py
@@ -13,15 +13,8 @@ class colors:  # You may need to change color settings
 
 URL = "https://www.emerita.legal/abogado/a/"
 
-headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET',
-    'Access-Control-Allow-Headers': 'Content-Type',
-    'Access-Control-Max-Age': '3600',
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-}
 
-req = requests.get(URL, headers)
-soup = BeautifulSoup(req.content, 'html.parser')
+response = requests.get(URL)
+soup = BeautifulSoup(response.content, 'html.parser')
 res = soup.find_all("li", class_="list-group-item")
 print(res)
--- python/scrapper/scrappertext.py
+++ python/scrapper/scrappertext.py
@@ -1,9 +1,21 @@
-# import requested for script work
-import json
 import requests
-import time
+
+cookies = {
+    '__stripe_mid': '0f483b43-a43f-4ec0-9f13-1da840fd90291a0508',
+    'cookieLawSeen': 'true',
+}
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:87.0) Gecko/20100101 Firefox/87.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Cache-Control': 'max-age=0',
+    'If-None-Match': 'W/"d7f6-LPeLItTxk4t2YdOsB2pWyzZcvHI-gzip"',
+}
 
 response = requests.get(
-    'https://www.emerita.legal/abogado/a')
-rawHTML = response.content
-print(rawHTML)
+    'https://www.emerita.legal/abogado/a', headers=headers, cookies=cookies)
+
+print(response.content)
